611a661f49454474a3b021dca3460b8ca056a399
Daniel Blanco <daniel.blancorojas@gmail.com>
2018-10-28 15:00:11 -0600

    Add a support file for tests

--- test/functional/webhook.spec.js
+++ test/functional/webhook.spec.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { browser } = require("../support");
 const { test, trait } = use("Test/Suite")("Webhook");
 
 trait("Test/Browser");
@@ -7,4 +8,4 @@ trait("Test/Browser");
 test("visit webhooks index page", async ({ browser }) => {
   const page = await browser.visit("/webhooks");
   await page.assertHas("Webhooks Index page!");
-}).timeout(0);
+}).timeout(browser.timeout);
